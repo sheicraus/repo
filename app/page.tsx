c355b515fbb19a92ef40a3e964e9e01179476e98
Sheila Mae Craus <68653710+sheicraus@users.noreply.github.com>
2023-10-30 18:42:05 +0800

    added myname :>

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import ChecklistForm from "@/app/components/checklist/ChecklistForm";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 /**
  * Landing page
@@ -6,11 +8,19 @@ import ChecklistForm from "@/app/components/checklist/ChecklistForm";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center p-5 h-screen w-screen bg-primaryBg">
-        <h1 className="font-bold text-6xl text-secondary-100">repo</h1>
-        <h2 className="font-medium text-lg text-center text-primaryText my-5">
-          Create, collaborate, and share checklists
-        </h2>
-        <ChecklistForm/>
+      <h1 className="font-bold text-6xl text-secondary-100">
+        repo
+      </h1>
+      <h2 className="font-medium text-lg text-center text-primaryText my-5">
+        Create, collaborate, and share checklists
+      </h2>
+
+      <ChecklistForm/>
+
+      <footer className="absolute bottom-10">
+        <FontAwesomeIcon fontSize={12} icon={faHeart} bounce className="text-secondary-50 mr-2" />
+        <span className="text-xs">Made with passion by Sheila Craus</span>
+      </footer>
     </main>
   );
 }
